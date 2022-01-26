825fed9f7cc94d5fa4f1e009d0b2c05149b81a10
현 재 <1234qkr@gmail.com>
2022-01-26 18:28:01 +0900

    finish fit_Line func

--- line.py
+++ line.py
@@ -67,13 +67,22 @@ def fit_line(img,r_lines,l_lines):
     #fitLine Func
     r_lines = cv2.fitLine(r_lines,cv2.DIST_L2,0,0.01,0.01,line = None)
     l_lines = cv2.fitLine(l_lines,cv2.DIST_L2,0,0.01,0.01)
+    print(r_lines,l_lines)
     #calculate line x,y points
     r_m = r_lines[1]/r_lines[0]
     l_m = l_lines[1]/l_lines[0]
     r_x , r_y = r_lines[2], r_lines[3]
     l_x , l_y = l_lines[2], l_lines[3]
+    y1 = int(img.shape[0])
+    y2 = int(img.shape[0]/2)
+    
+    rx_line1 = int((y1-r_y/r_m)+r_x)
+    rx_line2 = int((y2-r_y/r_m)+r_x)
+    lx_line1 = int((y1-l_y/l_m)+l_x)
+    lx_line2 = int((y2-l_y/l_m)+l_x)
 
-
-
-    print(r_lines)
-    return r_lines,l_lines
+    #return val r,l
+    points = []
+    points.append([rx_line1,y1,rx_line2,y2])
+    points.append([lx_line1,y1,lx_line2,y2])
+    return points
