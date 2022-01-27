93b7c8270e325b62cd3e1f1909455878e3b8d42b
현 재 <1234qkr@gmail.com>
2022-01-27 12:14:19 +0900

    fixing x2 point cal

--- line.py
+++ line.py
@@ -76,10 +76,10 @@ def fit_line(img,r_lines,l_lines):
     y1 = int(img.shape[0])
     y2 = int(img.shape[0]/2)
     rx_line1 = int(((y1-r_y)/r_m)+r_x)
-    print(y2,r_y,y2-r_y)
-    rx_line2 = int(((y2-r_y)/r_m)+r_x)
+    print(y2,r_y,r_y-y2)
+    rx_line2 = int(((r_y-y2)/r_m)+r_x)
     lx_line1 = int(((y1-l_y)/l_m)+l_x)
-    lx_line2 = int(((y2-l_y)/l_m)+l_x)
+    lx_line2 = int(((l_y-y2)/l_m)+l_x)
 
     #return val r,l
     points = []
